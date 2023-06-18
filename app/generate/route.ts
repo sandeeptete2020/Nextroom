d87979fcf480d97e3fa547d8b5fb49666077d359
sandeeptete2020 <119023082+sandeeptete2020@users.noreply.github.com>
2023-06-19 02:33:36 +0530

    Update route.ts

--- app/generate/route.ts
+++ app/generate/route.ts
@@ -49,7 +49,7 @@ export async function POST(request: Request) {
       input: {
         image: imageUrl,
         prompt:
-          "Beautiful Home Renovation, professional"
+          "Beautiful Home Renovation, professional",
        
       },
     }),
